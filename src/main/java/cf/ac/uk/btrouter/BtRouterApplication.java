9743c36422ed39f2584dc7256ba5aab7325c422f
c21079342 <pollyl@cardiff.ac.uk>
2025-01-29 16:58:31 +0000

    initial commit

---
+++ src/main/java/cf/ac/uk/btrouter/BtRouterApplication.java
@@ -0,0 +1,13 @@
+package cf.ac.uk.btrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtRouterApplication.class, args);
+    }
+
+}
