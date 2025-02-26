f753db9150db546c3cc7770d561860c5cf6b42e9
c21079342 <pollyl@cardiff.ac.uk>
2025-02-26 18:24:41 +0000

    list of users in admin pages

---
+++ src/main/java/cf/ac/uk/btrouter/controller/AdminController.java
@@ -0,0 +1,27 @@
+package cf.ac.uk.btrouter.controller;
+
+import cf.ac.uk.btrouter.model.User;
+import cf.ac.uk.btrouter.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+}
---
+++ src/main/java/cf/ac/uk/btrouter/service/UserService.java
@@ -0,0 +1,40 @@
+package cf.ac.uk.btrouter.service;
+
+import cf.ac.uk.btrouter.model.User;
+import cf.ac.uk.btrouter.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User registerUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already registered");
+        }
+
+        user.setRole(User.Role.USER);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
+
