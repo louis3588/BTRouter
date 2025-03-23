ba272007a9f1adf4baf2d447f75c48bea9cee841
c21079342 <pollyl@cardiff.ac.uk>
2025-03-23 17:33:03 +0000

    added user tests config

--- backend/src/main/java/cf/ac/uk/btrouter/controller/UserController.java
+++ backend/src/main/java/cf/ac/uk/btrouter/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
+
     public final UserService userService;
 
     public UserController(UserService userService) {
